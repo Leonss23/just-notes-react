e1f4e62a3d027a856ab86da92b403445a021b780
Leonardo Gatti <leonardogattidev@gmail.com>
2024-04-20 22:53:12 -0300

    NoteCard: bigger padding and gap

--- src/notes/NoteCard.tsx
+++ src/notes/NoteCard.tsx
@@ -7,9 +7,9 @@ type Props = {
 export default function NoteCard({ note }: Props) {
   return (
     <div
-      className={`flex flex-col rounded-xl p-2 shadow-md hover:hover:hover:bg-neutral-200 peer-checked:bg-white dark:bg-neutral-800 dark:hover:hover:hover:bg-neutral-700 dark:peer-checked:bg-neutral-900`}
+      className={`flex flex-col gap-2 rounded-xl p-4 shadow-md hover:hover:hover:bg-neutral-200 peer-checked:bg-white dark:bg-neutral-800 dark:hover:hover:hover:bg-neutral-700 dark:peer-checked:bg-neutral-900`}
     >
-      <div className="flex content-evenly items-center justify-between gap-6">
+      <div className="flex content-evenly items-start justify-between gap-6">
         <p className="text-2xl">{note.title}</p>
         <p className="text-sm text-neutral-500">
           {new Date(note.date).toLocaleDateString()}
